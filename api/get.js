a9afa5d938d0c5745e0997498af9282a022d347c
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-03-12 14:17:02 +0100

    starting off

--- api/get.js
+++ api/get.js
@@ -1,5 +1,7 @@
 module.exports = async (req, res) => {
-    if (req.method == "GET") {
-        res.send("OKAY")
-    }
+  if (req.method == "GET") {
+    res.send(
+      "As we all stand on the shoulders of giants, tomorrow I hope to be the same for you."
+    );
+  }
 };
