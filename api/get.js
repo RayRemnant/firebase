146354a841f9493c9449b62531868b39f5150005
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-03-24 15:37:56 +0100

    removing unnecessary files

--- api/get.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method == "GET") {
-    res.send(
-      "As we all stand on the shoulders of giants, tomorrow I hope to be the same for you."
-    );
-  }
-};
